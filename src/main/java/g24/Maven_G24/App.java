3da946623bbcb8382bb7d21f11ffa13b4938a115
OksanaAleksn <axksyu@gmail.com>
2017-05-03 00:14:26 +0300

    mvn24_commit_02_05

---
+++ src/main/java/g24/Maven_G24/App.java
@@ -0,0 +1,13 @@
+package g24.Maven_G24;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/g24/Maven_G24/JunitMaven.java
@@ -0,0 +1,15 @@
+package g24.Maven_G24;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class JunitMaven {
+	@Test
+	public void firstMavenTest(){
+		WebDriver m_driver = new ChromeDriver();
+		WebElement testElt;
+	}
+
+}
